9e0155953e0a3e281f3ca1b6df9f54ed9ca10432
IrfanHidayatulahP <irfanhputra400@gmail.com>
2024-11-22 01:48:06 +0700

    Membuat Data Object MataKuliah

---
+++ app/src/main/java/com/example/quest6/data/MataKuliah.kt
@@ -0,0 +1,12 @@
+package com.example.quest6.data
+
+object MataKuliah {
+    val option = listOf(
+        "Pemrogaman Aplikasi Mobile Lanjut",
+        "Pemrogaman Aplikasi Web Framework",
+        "Prototyping",
+        "Aplikasi Multimedia",
+        "Kecerdasan Buatan",
+        "Computer Vission"
+    )
+}
