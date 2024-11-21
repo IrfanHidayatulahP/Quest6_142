98cba5cf3cf2d30431cd80e05cc2af332d69ddf0
IrfanHidayatulahP <irfanhputra400@gmail.com>
2024-11-22 02:12:03 +0700

    membuat variable expanded, dan menambahkan parameter selectedValue, options, label, onValueChangedEvent, dan modifier

---
+++ app/src/main/java/com/example/quest6/widget/DynamicSelectedField.kt
@@ -0,0 +1,21 @@
+package com.example.quest6.widget
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DynamicSelectedTextField(
+    selectedValue: String,
+    options: List<String>,
+    label: String,
+    onValueChangedEvent: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    var expanded by remember { mutableStateOf((false)) }
+}
